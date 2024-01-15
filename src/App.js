afbe44549182adda553b06b979fff07eb6da69bd
Joao Pedro Santos <joaosilvasantos1702@gmail.com>
2024-01-15 18:36:07 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World
+          Primeira mudança no projeto
         </p>
         <a
           className="App-link"
